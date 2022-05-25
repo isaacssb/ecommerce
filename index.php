1f4a2a2f7f0ce451828f191321aa1715e5e18c66
Isaac Barreto <isaacssb123@gmail.com>
2022-05-24 21:12:58 -0300

    aula 109

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
-use  \Hcode\Model\User;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -201,6 +202,81 @@ $app->post('/admin/forgot/reset', function(){
 		"name" => $user["desperson"],
 		"code" => $_GET["code"]
 	));
+});
+
+$app->get('/admin/categories', function() {
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	$categories = Category::listAll();
+
+	$page->setTpl("categories", array(
+		'categories' => $categories
+	));
+
+});
+
+$app->get('/admin/categories/create', function() {
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post('/admin/categories/create', function() {
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory) {
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory) {
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", array(
+		"category" => $category->getValues()
+	));
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory) {
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
 });
 
 
